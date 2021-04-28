55c73bb722fc3dfc2a051e10c27caf3aad528b35
triveni07-codes <triveni.wadkar@gmail.com>
2021-04-28 23:37:53 +0200

    Add spring initializer files

---
+++ message-board-api/src/main/java/com/assignment/messageboard/messageboardapi/MessageBoardApiApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.messageboard.messageboardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageBoardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageBoardApiApplication.class, args);
+	}
+
+}
---
+++ message-board-api/src/test/java/com/assignment/messageboard/messageboardapi/MessageBoardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.messageboard.messageboardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageBoardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
