cd6d6ac910a741b4828cf1be37dfc0d385e2f9e5
triveni07/codes <triveni.wadkar@gmail.com>
2021-05-02 14:51:16 +0200

    Add cross origin to make proxy available to frontend

--- message-board-api/src/main/java/com/assignment/messageboardapi/controller/MessageBoardController.java
+++ message-board-api/src/main/java/com/assignment/messageboardapi/controller/MessageBoardController.java
@@ -12,8 +12,10 @@ import javax.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @Slf4j
 public class MessageBoardController implements MessageBoardApi {
